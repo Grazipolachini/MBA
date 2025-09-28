712c04ba174d67989d8cd0d9fa3077792ef89388
Grazipolachini <grazi.polachini@gmail.com>
2025-09-28 17:48:30 -0300

    Create variables.tf

---
+++ trabalho_iac/variables.tf
@@ -0,0 +1,59 @@
+variable "project_name" {
+  type    = string
+  default = "iac-trabalho"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "availability_zones" {
+  type    = list(string)
+  default = [] # se vazio, pega 2 AZs da região
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "min_size" {
+  type    = number
+  default = 1
+}
+
+variable "max_size" {
+  type    = number
+  default = 2
+}
+
+variable "desired_capacity" {
+  type    = number
+  default = 1
+}
+
+variable "ssh_allow_cidr" {
+  type    = string
+  default = "0.0.0.0/0"
+}
+
+variable "create_route53" {
+  type    = bool
+  default = false
+}
+
+variable "route53_zone_id" {
+  type    = string
+  default = ""
+}
+
+variable "domain_name" {
+  type    = string
+  default = ""
+}
