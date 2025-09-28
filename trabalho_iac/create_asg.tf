ff7a09c5c1cf4fd084b6faf0394722732106053b
Grazipolachini <grazi.polachini@gmail.com>
2025-09-28 17:54:58 -0300

    Create create_asg.tf

---
+++ trabalho_iac/create_asg.tf
@@ -0,0 +1,62 @@
+resource "aws_security_group" "ec2_sg" {
+  name   = "${var.project_name}-ec2-sg"
+  vpc_id = aws_vpc.this.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_allow_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+  }
+}
+
+resource "aws_launch_template" "app" {
+  name_prefix   = "${var.project_name}-lt-"
+  image_id      = data.aws_ami.amazon_linux_2.id
+  instance_type = var.instance_type
+
+  network_interfaces {
+    security_groups             = [aws_security_group.ec2_sg.id]
+    associate_public_ip_address = true
+  }
+
+  user_data = base64encode(file("${path.module}/userdata.sh"))
+}
+
+resource "aws_autoscaling_group" "asg" {
+  name                      = "${var.project_name}-asg"
+  desired_capacity          = var.desired_capacity
+  min_size                  = var.min_size
+  max_size                  = var.max_size
+  vpc_zone_identifier       = [for s in aws_subnet.public : s.id]
+  launch_template {
+    id      = aws_launch_template.app.id
+    version = "$Latest"
+  }
+  target_group_arns         = [aws_lb_target_group.app_tg.arn]
+  health_check_type         = "ELB"
+  health_check_grace_period = 60
+}
