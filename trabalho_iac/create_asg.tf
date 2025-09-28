b69ff25fff62f56eb9cd6ef707ce7cfabbae1992
Grazipolachini <grazi.polachini@gmail.com>
2025-09-28 19:16:13 -0300

    Update create_asg.tf
    ajuste de tags das instancias

--- trabalho_iac/create_asg.tf
+++ trabalho_iac/create_asg.tf
@@ -1,3 +1,4 @@
+# Security Group para EC2
 resource "aws_security_group" "ec2_sg" {
   name   = "${var.project_name}-ec2-sg"
   vpc_id = aws_vpc.this.id
@@ -24,15 +25,18 @@ resource "aws_security_group" "ec2_sg" {
   }
 }
 
+# Data source para pegar a AMI mais recente do Amazon Linux 2
 data "aws_ami" "amazon_linux_2" {
   most_recent = true
   owners      = ["amazon"]
+
   filter {
     name   = "name"
     values = ["amzn2-ami-hvm-*-x86_64-ebs"]
   }
 }
 
+# Launch Template para as instâncias EC2
 resource "aws_launch_template" "app" {
   name_prefix   = "${var.project_name}-lt-"
   image_id      = data.aws_ami.amazon_linux_2.id
@@ -44,19 +48,37 @@ resource "aws_launch_template" "app" {
   }
 
   user_data = base64encode(file("${path.module}/userdata.sh"))
+
+  # Tag para instâncias criadas pelo Launch Template
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "iac-trabalho-ec2"
+    }
+  }
 }
 
+# Auto Scaling Group
 resource "aws_autoscaling_group" "asg" {
   name                      = "${var.project_name}-asg"
   desired_capacity          = var.desired_capacity
   min_size                  = var.min_size
   max_size                  = var.max_size
   vpc_zone_identifier       = [for s in aws_subnet.public : s.id]
+
   launch_template {
     id      = aws_launch_template.app.id
     version = "$Latest"
   }
+
   target_group_arns         = [aws_lb_target_group.app_tg.arn]
   health_check_type         = "ELB"
   health_check_grace_period = 60
+
+  # Tag para instâncias criadas pelo ASG
+  tag {
+    key                 = "Name"
+    value               = "iac-trabalho-ec2"
+    propagate_at_launch = true
+  }
 }
